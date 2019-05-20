6a59777e5cd356fd211914b8033e1055566651c7
Pierreleproducode <pierre.laurend@live.fr>
2019-05-20 16:02:46 +0200

    création du git pour ce projet Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'BidocheMan' },
+  { id: 12, name: 'Bertrand du bistrot d à coté' },
+  { id: 13, name: 'Denis Brogniard' },
+  { id: 14, name: 'Nikos' },
+  { id: 15, name: 'Messi' },
+  { id: 16, name: 'JavaEEman' },
+  { id: 17, name: 'Flo libre le sauveur' },
+  { id: 18, name: 'peneloppe' },
+  { id: 19, name: 'Tao ' },
+  { id: 20, name: 'Arya' }
+];
